879f0b985780e193a7596234a9fa05e46d6d2ea6
Tauhid Malik <tauhid@Mac.attlocal.net>
2026-07-23 20:58:38 -0700

    feat: configure custom SES mail-from domain

--- lib/constructs/auth.ts
+++ lib/constructs/auth.ts
@@ -40,6 +40,7 @@ export class CognitoAuth extends Construct {
       'SenderEmailIdentity',
       {
         identity: Identity.email('noreply@prokuro.ai'),
+        mailFromDomain: 'main.prokuro.ai',
       },
     );
 
