82249e9cefd3e6ada29420f1307a6abb986a2202
moonneer <metamorphicex@gmail.com>
2026-07-23 18:04:44 -0700

    feat: configure Google authentication

--- lib/config.ts
+++ lib/config.ts
@@ -6,10 +6,15 @@ export const DIGIKEY_SECRET_NAME = 'prokuro/digikey/credentials';
 /** Secrets Manager name for GitHub PAT (Amplify repo access). */
 export const GITHUB_SECRET_NAME = 'prokuro/github/amplify-token';
 
+/** Secrets Manager JSON secret with client_id and client_secret. */
+export const GOOGLE_OAUTH_SECRET_NAME = 'prokuro/google/oauth';
+
 export const GITHUB_OWNER = 'prokuro-ai';
 export const GITHUB_REPO = 'prokuroWeb';
 export const PRODUCTION_BRANCH = 'main';
-
+export const COGNITO_DOMAIN_PREFIX = 'prokuro-auth';
+export const WEB_BASE_URL =
+  'https://main.d1kbe3qii40ozr.amplifyapp.com';
 
 export const BACKEND_DIR = join(__dirname, '../../../prokuroBackend');
 
--- lib/constructs/auth.ts
+++ lib/constructs/auth.ts
@@ -1,20 +1,30 @@
-import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, Stack } from 'aws-cdk-lib';
 import {
   AccountRecovery,
   CfnUserPool,
-  CfnUserPoolClient,
   Mfa,
+  OAuthScope,
+  ProviderAttribute,
   StringAttribute,
   UserPool,
   UserPoolClient,
+  UserPoolClientIdentityProvider,
+  UserPoolIdentityProviderGoogle,
 } from 'aws-cdk-lib/aws-cognito';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
+import {
+  COGNITO_DOMAIN_PREFIX,
+  GOOGLE_OAUTH_SECRET_NAME,
+  WEB_BASE_URL,
+} from '../config';
 
 export class CognitoAuth extends Construct {
   readonly userPool: UserPool;
   readonly userPoolClient: UserPoolClient;
   readonly userPoolId: string;
   readonly userPoolClientId: string;
+  readonly domainName: string;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -33,6 +43,9 @@ export class CognitoAuth extends Construct {
         company: new StringAttribute({ mutable: true }),
       },
       passwordPolicy: { minLength: 8 },
+      signInPolicy: {
+        allowedFirstAuthFactors: { password: true, emailOtp: true },
+      },
       accountRecovery: AccountRecovery.EMAIL_ONLY,
       mfa: Mfa.OFF,
       removalPolicy: RemovalPolicy.DESTROY,
@@ -41,23 +54,62 @@ export class CognitoAuth extends Construct {
 
     const cfnUserPool = this.userPool.node.defaultChild as CfnUserPool;
     cfnUserPool.usernameConfiguration = { caseSensitive: false };
-    cfnUserPool.addPropertyOverride('Policies.SignInPolicy', {
-      AllowedFirstAuthFactors: ['PASSWORD', 'EMAIL_OTP'],
+
+    const googleOAuthSecret = Secret.fromSecretNameV2(
+      this,
+      'GoogleOAuthSecret',
+      GOOGLE_OAUTH_SECRET_NAME,
+    );
+    const googleProvider = new UserPoolIdentityProviderGoogle(
+      this,
+      'GoogleIdentityProvider',
+      {
+        userPool: this.userPool,
+        clientId: googleOAuthSecret
+          .secretValueFromJson('client_id')
+          .unsafeUnwrap(),
+        clientSecretValue:
+          googleOAuthSecret.secretValueFromJson('client_secret'),
+        scopes: ['openid', 'email', 'profile'],
+        attributeMapping: {
+          email: ProviderAttribute.GOOGLE_EMAIL,
+          emailVerified: ProviderAttribute.GOOGLE_EMAIL_VERIFIED,
+          givenName: ProviderAttribute.GOOGLE_GIVEN_NAME,
+          familyName: ProviderAttribute.GOOGLE_FAMILY_NAME,
+        },
+      },
+    );
+
+    const domain = this.userPool.addDomain('Domain', {
+      cognitoDomain: { domainPrefix: COGNITO_DOMAIN_PREFIX },
     });
+    this.domainName = `${domain.domainName}.auth.${Stack.of(this).region}.amazoncognito.com`;
 
     this.userPoolClient = this.userPool.addClient('WebClient', {
       userPoolClientName: 'prokuro-web',
-      authFlows: { userPassword: true, userSrp: true },
+      // The existing passwordless email flow uses USER_AUTH with EMAIL_OTP.
+      authFlows: { user: true, userPassword: true, userSrp: true },
       generateSecret: false,
+      supportedIdentityProviders: [
+        UserPoolClientIdentityProvider.COGNITO,
+        UserPoolClientIdentityProvider.GOOGLE,
+      ],
+      oAuth: {
+        callbackUrls: [
+          `${WEB_BASE_URL}/auth/callback`,
+          'http://localhost:3010/auth/callback',
+        ],
+        logoutUrls: [`${WEB_BASE_URL}/`, 'http://localhost:3010/'],
+        flows: { authorizationCodeGrant: true },
+        scopes: [
+          OAuthScope.OPENID,
+          OAuthScope.EMAIL,
+          OAuthScope.PROFILE,
+          OAuthScope.COGNITO_ADMIN,
+        ],
+      },
     });
-
-    const cfnClient = this.userPoolClient.node.defaultChild as CfnUserPoolClient;
-    cfnClient.explicitAuthFlows = [
-      'ALLOW_REFRESH_TOKEN_AUTH',
-      'ALLOW_USER_AUTH',
-      'ALLOW_USER_PASSWORD_AUTH',
-      'ALLOW_USER_SRP_AUTH',
-    ];
+    this.userPoolClient.node.addDependency(googleProvider);
 
     this.userPoolId = this.userPool.userPoolId;
     this.userPoolClientId = this.userPoolClient.userPoolClientId;
@@ -72,6 +124,11 @@ export class CognitoAuth extends Construct {
       value: this.userPoolClientId,
       description: 'Cognito app client ID',
       exportName: 'ProkuroCognitoClientId',
+    });
+
+    new CfnOutput(this, 'GoogleOAuthRedirectUri', {
+      value: `https://${this.domainName}/oauth2/idpresponse`,
+      description: 'Authorized redirect URI for the Google OAuth client',
     });
   }
 }
